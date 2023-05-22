71ee7c6f45591f776acd75c5034bfb93d1c7494b
rostiklviv <r.romanko@nltu.lviv.ua>
2023-05-23 00:58:13 +0300

    test commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import React, { useCallback } from 'react';
-import GoogleButton from 'react-google-button';
-
-const { REACT_APP_GOOGLE_CLIENT_ID, REACT_APP_BASE_BACKEND_URL } = process.env;
+import Login from './pages/Login';
 
 function App() {
-
-  const openGoogleLoginPage = useCallback(() => {
-    const googleAuthUrl = 'https://accounts.google.com/o/oauth2/v2/auth';
-    const redirectUri = 'accounts/google/login/callback/';
-
-    const scope = [
-      'https://www.googleapis.com/auth/userinfo.email',
-      'https://www.googleapis.com/auth/userinfo.profile'
-    ].join(' ');
-
-    const params = {
-      response_type: 'code',
-      client_id: REACT_APP_GOOGLE_CLIENT_ID,
-      redirect_uri: `${REACT_APP_BASE_BACKEND_URL}/${redirectUri}`,
-      prompt: 'select_account',
-      access_type: 'offline',
-      scope
-    };
-
-    const urlParams = new URLSearchParams(params).toString();
-
-    window.location = `${googleAuthUrl}?${urlParams}`;
-  }, []);
   
   return (
-    <div className="App">
-      <GoogleButton
-        onClick={openGoogleLoginPage}
-        label="Sign in with Google"
-        disabled={!REACT_APP_GOOGLE_CLIENT_ID}
-      />
-    </div>
+    <Routes>
+      <Route path='/login' element={<Login />}></Route>
+    </Routes>
   );
 }
 
---
+++ src/pages/Login.js
@@ -0,0 +1,42 @@
+import React, { useCallback } from 'react';
+import GoogleButton from 'react-google-button';
+
+const { REACT_APP_GOOGLE_CLIENT_ID, REACT_APP_BASE_BACKEND_URL } = process.env;
+
+const Login = () => {
+
+    const openGoogleLoginPage = useCallback(() => {
+        const googleAuthUrl = 'https://accounts.google.com/o/oauth2/v2/auth';
+        const redirectUri = 'accounts/google/login/callback/';
+    
+        const scope = [
+          'https://www.googleapis.com/auth/userinfo.email',
+          'https://www.googleapis.com/auth/userinfo.profile'
+        ].join(' ');
+    
+        const params = {
+          response_type: 'code',
+          client_id: REACT_APP_GOOGLE_CLIENT_ID,
+          redirect_uri: `${REACT_APP_BASE_BACKEND_URL}/${redirectUri}`,
+          prompt: 'select_account',
+          access_type: 'offline',
+          scope
+        };
+    
+        const urlParams = new URLSearchParams(params).toString();
+    
+        window.location = `${googleAuthUrl}?${urlParams}`;
+      }, []);
+
+    return ( 
+        <div className="App">
+            <GoogleButton
+                onClick={openGoogleLoginPage}
+                label="Sign in with Google"
+                disabled={!REACT_APP_GOOGLE_CLIENT_ID}
+            />
+        </div>
+     );
+}
+ 
+export default Login;
