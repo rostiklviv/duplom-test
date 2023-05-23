161c7f3ee591fd0f9e897ac3df688004164e5907
rostiklviv <r.romanko@nltu.lviv.ua>
2023-05-24 00:42:23 +0300

    asd

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,8 @@ const Home = () => {
 
     function handleClick() {
         fetch('http://localhost:8000/api/chats/', {
-            credentials: 'same-origin',
-            headers: { "Content-Type": "application/json" },
-        }).then(response => response.json())
-        .then(data => this.setState({ totalReactPackages: data.total }));
+            credentials: 'include'
+        })
     }
 
     return ( 
